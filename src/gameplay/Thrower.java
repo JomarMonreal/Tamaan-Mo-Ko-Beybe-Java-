cae1ff33e1479f5e325d18c297bdfac013636db9
JomarMonreal <jpmonreal2035@gmail.com>
2023-12-18 00:33:22 +0800

    Update Thrower.java

--- src/gameplay/Thrower.java
+++ src/gameplay/Thrower.java
@@ -1,12 +1,22 @@
+/*************************************************************************************************************************
+ *
+ * Thrower
+ * The one who throws the ball
+ * 
+ * @author Jomar Monreal, Alessandro Marcus Ocampo, James Carl Villarosa
+ * @date 2023-12-18 
+ *************************************************************************************************************************/
+
 package gameplay;
 
 import javafx.scene.image.Image;
 
 public class Thrower extends Sprite{
-	
+	//props
 	private double speed;
 	private boolean isHoldingBall;
-
+	
+	//constructor
 	public Thrower(double xPos, double yPos, double size, Image image,double speed,boolean isHoldingBall) {
 		super(xPos,yPos,size,size,image);
 		this.setSpeed(speed);
