f8294d7b73bed59a7c929bc86e241a8596cb9259
JomarMonreal <jpmonreal2035@gmail.com>
2023-12-18 00:33:38 +0800

    Update Ball.java

--- src/gameplay/Ball.java
+++ src/gameplay/Ball.java
@@ -1,20 +1,35 @@
+/*************************************************************************************************************************
+ *
+ * Ball 
+ * The object being thrown by the thrower and must be dodged by the dodger
+ * 
+ * @author Jomar Monreal, Alessandro Marcus Ocampo, James Carl Villarosa
+ * @date 2023-12-18 
+ *************************************************************************************************************************/
+
+
 package gameplay;
 
 import javafx.scene.image.Image;
 
 public class Ball extends Sprite{
+	//normalized vector components for movement
 	private double normalizedVectorX = 0;
 	private double normalizedVectorY = 0;
+	
+	//common props
 	private boolean isMoving = false;
 	private double radius;
 	private double speed = 8;
-
+	
+	//constructor
 	public Ball(double xPos, double yPos, double radius,Image img,double speed) {
 		super(xPos, yPos, radius, radius,img);
 		this.radius = radius;
 		this.setSpeed(speed);
 	}
 	
+	//set normalized vector based on mouse position
 	public void setNormalizedVector(double mousePosX, double mousePosY) {
 	 
 		// find direction of ball thrown when mouse clicked  
