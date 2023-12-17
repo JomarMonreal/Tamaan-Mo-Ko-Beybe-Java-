28db75986bc6b27075efb76f29ea676741370700
JomarMonreal <jpmonreal2035@gmail.com>
2023-12-18 00:33:27 +0800

    Update Sprite.java

--- src/gameplay/Sprite.java
+++ src/gameplay/Sprite.java
@@ -1,3 +1,13 @@
+/*************************************************************************************************************************
+ *
+ * Sprite
+ * The base class for scene objects that needs collision detection
+ * 
+ * @author Jomar Monreal, Alessandro Marcus Ocampo, James Carl Villarosa
+ * @date 2023-12-18 
+ *************************************************************************************************************************/
+
+
 package gameplay;
 
 import javafx.scene.image.Image;
@@ -5,32 +15,36 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.geometry.Rectangle2D;
 
 public class Sprite {
+	//props
 	protected Image img;
 	protected double xPos, yPos, dx, dy;
 	protected boolean visible;
 	protected double width;
 	protected double height;
 	protected boolean isShorter = false;
-
+	
+	//constructor
     public Sprite(double xPos, double yPos, double width, double height, Image image){
 		this.xPos = xPos;
 		this.yPos = yPos;
 		this.width = width;
 		this.height = height;
-		this.loadImage(image);
+		this.setImage(image);
 		this.visible = true;
 	}
     
+    //constructor with shorter property (for smaller collision box)
     public Sprite(double xPos, double yPos, double width, double height, Image image, boolean isShorter){
 		this.xPos = xPos;
 		this.yPos = yPos;
 		this.width = width;
 		this.height = height;
-		this.loadImage(image);
+		this.setImage(image);
 		this.visible = true;
 		this.isShorter = isShorter;
 	}
-
+    
+    //get bounds
 	private Rectangle2D getBounds(boolean isShorter){
 		if(isShorter) {
 			return new Rectangle2D(this.xPos + this.width/4, this.yPos+ this.height/4, this.width/2, this.height/2);
@@ -38,21 +52,15 @@ public class Sprite {
 		return new Rectangle2D(this.xPos, this.yPos, this.width, this.height);
 	}
 	
+	//check collision
 	protected boolean collidesWith(Sprite rect2)	{
 		Rectangle2D rectangle1 = this.getBounds(true);
 		Rectangle2D rectangle2 = rect2.getBounds(this.isShorter);
 
 		return rectangle1.intersects(rectangle2);
 	}
-
-	protected void loadImage(Image image){
-		try{
-			this.img = image;
-		} catch(Exception e)	{
-			e.printStackTrace();
-		}
-	}
 	
+	//render image
 	public void render(GraphicsContext gc){
         gc.drawImage( this.img, this.xPos, this.yPos, this.width, this.height );
     }
