829b59f13934e2f2ca3ec9a1a7f1f2bf3635167a
JomarMonreal <jpmonreal2035@gmail.com>
2023-12-18 00:33:24 +0800

    Update Taya.java

--- src/gameplay/Taya.java
+++ src/gameplay/Taya.java
@@ -1,22 +1,35 @@
+/*************************************************************************************************************************
+ *
+ * Taya
+ * The one who dodges the ball
+ * 
+ * @author Jomar Monreal, Alessandro Marcus Ocampo, James Carl Villarosa
+ * @date 2023-12-18 
+ *************************************************************************************************************************/
+
 package gameplay;
 
 import javafx.scene.image.Image;
 
 public class Taya extends Sprite{
+	//images for each state
 	private Image harap;
 	private Image likod;
 	private Image left;
 	private Image right;
 	private Image gotHit;
 	
+	//speed
 	private double speed;
 	
+	//constructor
 	public Taya(double xPos, double yPos, double width, double height, Image image, double speed) {
 		super(xPos,yPos,width,height,image,true);
 		this.speed = speed;
 		this.isShorter = true;
 	}
 	
+	//initialize images for different directions
 	public void initDirectionalImages(Image harap,Image likod,Image left, Image right,Image gotHit) {
 		this.harap = harap;
 		this.likod = likod;
