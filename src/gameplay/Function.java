019962598ffd42b69900c3749596fd0c318123bf
JomarMonreal <jpmonreal2035@gmail.com>
2023-12-18 00:33:36 +0800

    Update Function.java

--- src/gameplay/Function.java
+++ src/gameplay/Function.java
@@ -1,3 +1,13 @@
+/*************************************************************************************************************************
+ *
+ * Function interface 
+ * Acts as a function storage (delegate keyword in C#)
+ * Follows command design pattern
+ * 
+ * @author Jomar Monreal, Alessandro Marcus Ocampo, James Carl Villarosa
+ * @date 2023-12-18 
+ *************************************************************************************************************************/
+
 package gameplay;
 
 public interface Function {
